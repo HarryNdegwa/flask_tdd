9746e52d96917ff4471480bbbb6aa68552553f68
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-17 23:17:37 +0300

    Added testing and seriuos stuff logic

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+testing = os.environ.get("TESTING")
+
+database = None
+
+if testing:
+    print("Testing")
+    # database = app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:harry1998@127.0.0.1:5432/test_db"
+else:
+    print("Serious stuff")
+    # database = app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:harry1998@127.0.0.1:5432/prod_db"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
