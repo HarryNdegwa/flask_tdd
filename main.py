a603f0e38dc71c4491addc7ebf87ec9278994b36
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-18 18:02:47 +0300

    Added bcrypt stuff

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 
@@ -16,6 +18,8 @@ else:
     db_config = app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:harry1998@127.0.0.1:5432/flask_tdd"
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -14,11 +14,12 @@ class User(db.Model):
     password = db.Column(db.String,nullable=False)
 
 
-    def __init__(self,username,first_name,last_name,email):
+    def __init__(self,username,first_name,last_name,email,password):
         self.username = username
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.password = ""
 
 
 
