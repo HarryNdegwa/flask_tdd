0838f0f11d658ef2815f9eee37b95364174da879
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-20 15:33:02 +0300

    Modified post owner foreign key

--- resources/post_resource.py
+++ resources/post_resource.py
@@ -8,7 +8,7 @@ class Post(db.Model):
     __tablename__ = "posts"
 
     id = db.Column(db.Integer,primary_key=True)
-    owner_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    owner_id = db.Column(db.Integer,db.ForeignKey("users.id"))
     content = db.Column(db.String,nullable=False)
     created_at = db.Column(db.DateTime,default=datetime.utcnow)
 
