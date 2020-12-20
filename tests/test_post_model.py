3a094f78125f1594722f3fa2da06c9b3dd803153
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-20 10:35:40 +0300

    Added init code in post test

--- tests/test_post_model.py
+++ tests/test_post_model.py
@@ -4,7 +4,7 @@ class PostModelTestCase(BaseCase):
 
     def setUp(self):
         super().setUp()
-        self.post = self.Post()
+        self.post = self.Post("Test post")
 
     def test_post_str(self):
         self.assertEqual(str(self.post),"Test post")
