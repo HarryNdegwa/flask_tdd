4170694acd9203b44d67695746568734d44c1638
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-20 17:29:36 +0300

    Added post resource test case

---
+++ tests/test_post_resource.py
@@ -0,0 +1,10 @@
+from tests.base import BaseCase
+
+class PostResourceTestCase(BaseCase):
+
+    def setUp(self):
+        super().setUp()
+        
+
+    def test_post_creation(self):
+        pass
