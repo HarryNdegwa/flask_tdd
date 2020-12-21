6145bc3ea85b9b441edd088fc158b2d175267f51
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-21 17:13:34 +0300

    Added token expires in test user model setup

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -7,6 +7,7 @@ class UserModelTestCase(BaseCase):
     def setUp(self):
         super().setUp()
         self.user = self.User("Ndech","Harrison","Ndegwa","harryndegwa4@gmail.com","testuser")
+        self.token_expires = 5 #seconds
 
     
     def test_user_instance(self):
@@ -27,31 +28,28 @@ class UserModelTestCase(BaseCase):
 
     def test_encode_user_token(self):
         user_id = 1
-        token_validity = 120 # seconds
-        token = self.encode_user_token(user_id,token_validity,self.jwt_secret).decode()
+        token = self.user.encode_user_token(user_id,self.token_expires,self.jwt_secret).decode()
         token_sections = token.split(".")
         self.assertEqual(len(token_sections),3)
 
 
     def test_decode_valid_user_token(self):
         user_id = 1
-        token_validity = 120 # seconds
-        token = self.encode_user_token(user_id,token_validity,self.jwt_secret).decode()
+        token = self.user.encode_user_token(user_id,self.token_expires,self.jwt_secret).decode()
         token_sections = token.split(".")
         self.assertEqual(len(token_sections),3)
-        decoded_token = self.decode_user_token(token,self.jwt_secret)
+        decoded_token = self.user.decode_user_token(token,self.jwt_secret)
         self.assertIn("sub",decoded_token.keys())
         self.assertEqual(decoded_token.get("sub"),user_id)
 
 
     def test_decode_invalid_signature_user_token(self):
         user_id = 1
-        token_validity = 120 # seconds
-        token = self.encode_user_token(user_id,token_validity,self.jwt_secret).decode()
+        token = self.user.encode_user_token(user_id,self.token_expires,self.jwt_secret).decode()
         token_sections = token.split(".")
         self.assertEqual(len(token_sections),3)
         unknown_jwt_secret = "\xd1\xd7\xee_\xab\xd0UB:\x18\x1bh8\xc8\x90\x0eb+"
-        decoded_token = self.decode_user_token(token,unknown_jwt_secret)
+        decoded_token = self.user.decode_user_token(token,unknown_jwt_secret)
         self.assertIsInstance(decoded_token,str)
         self.assertEqual(decoded_token,"Invalid token. Please login again!")
         
@@ -59,12 +57,11 @@ class UserModelTestCase(BaseCase):
 
     def test_decode_expired_user_token(self):
         user_id = 1
-        token_validity = 120 # seconds
-        token = self.encode_user_token(user_id,token_validity,self.jwt_secret).decode()
+        token = self.user.encode_user_token(user_id,self.token_expires,self.jwt_secret).decode()
         token_sections = token.split(".")
         self.assertEqual(len(token_sections),3)
-        time.sleep(180)
-        decoded_token = self.decode_user_token(token,self.jwt_secret)
+        time.sleep(10)
+        decoded_token = self.user.decode_user_token(token,self.jwt_secret)
         self.assertIsInstance(decoded_token,str)
         self.assertEqual(decoded_token,"Token expired. Please login again!")
 
