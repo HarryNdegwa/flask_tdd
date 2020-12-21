d6c2d2998c128fbc334908f76668fd7e4e8b1051
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-21 15:55:06 +0300

    Modified config .py

--- app.py
+++ app.py
@@ -11,21 +11,17 @@ from config import *
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-
 app_env = os.environ.get("FLASK_ENV")
 
 
 if app_env == "testing":
     app.config.from_object(TestingConfig())
-    print(app.config)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:harry1998@127.0.0.1:5432/test"
+    
 elif app_env == "development":
     app.config.from_object(DevelopmentConfig())
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:harry1998@127.0.0.1:5432/flask_tdd"
+
 elif app_env == "production":
     app.config.from_object(ProductionConfig())
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:harry1998@127.0.0.1:5432/flask_tdd"
 
 
 db = SQLAlchemy(app)
