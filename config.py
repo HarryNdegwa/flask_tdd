9638876b94bddd9f2bda4aa19c77ffc23f758465
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-21 15:19:12 +0300

    Added config file

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = 'sqlite:///:memory:'
+    JWT_SECRET = "\xd1\xd7\xee_\xab\xd0UB:\x18\x1bh8\xc8\x90\x0eb+\xc67R\xec^\x90"
+    TOKEN_EXPIRES = 300  # in seconds
+
+class ProductionConfig(Config):
+    DATABASE_URI = 'mysql://user@localhost/foo'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
