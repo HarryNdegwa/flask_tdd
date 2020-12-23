38b71b93949e7d14772728241492bf3ab7d883d0
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-12-23 15:17:40 +0300

    Modified config token expires

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:harry1998@127.0.0.1:5432/flask_tdd'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     JWT_SECRET = "\xd1\xd7\xee_\xab\xd0UB:\x18\x1bh8\xc8\x90\x0eb+\xc67R\xec^\x90"
-    TOKEN_EXPIRES = 300  # in seconds
+    TOKEN_EXPIRES = 3600  # in seconds
 
 
 class ProductionConfig(Config):
